b48d91e4a176bd697c2064475b0b3159d48d43cc
Jasper Janssens <jasperjanssens5@gmail.com>
2026-02-03 20:09:47 +0100

    Remove _archive folder from repository
    The archive folder contains old development files and should not be in the repository

--- _archive/fragments_conversion/subsample_script.sh
+++
@@ -1,5 +0,0 @@
-ls -lah fragment_files/enterocytes/bonobo_enterocytes.fragments.tsv.gz
-
-mkdir subsampled_fragments/enterocytes
-
-cat -n 100 fragment_files/enterocytes/bonobo_enterocytes.fragments.tsv.gz > subsampled_fragments/enterocytes/bonobo_enterocytes.fragments.tsv.gz
